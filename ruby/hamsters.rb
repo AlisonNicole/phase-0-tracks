7f406160e3e97e7e081e33755d9b0838da145f4f
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-03 16:17:28 -0700

    Completed hamsters

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,9 +1,10 @@
-
 puts "What is the hamster's name?"
 name = gets.chomp
+name.to_s
 
 puts "What is the volume level? (1-10)"
 volume = gets.chomp
+volume.to_f
 
 puts "What color is the hamster?"
 color = gets.chomp
@@ -13,8 +14,17 @@ adoption = gets.chomp
 
 puts "What is the estimated age of the hamster?"
 age = gets.chomp
+age.to_f
 
 if age.empty?
-	puts "age unknown"
+	#age.to_s
+	age = "age unknown"
 end
+
+puts "Your hamster information"
+puts "Name: #{name}"
+puts "Volume level: #{volume}"
+puts "Color: #{color}"
+puts "Good candidate for Adoption? #{adoption}"
+puts "Estimated Age: #{age}"
 
