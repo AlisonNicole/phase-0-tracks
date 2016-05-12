04edfab9f730f588b92fdf41d3d46b21152f4858
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-11 23:26:37 -0700

    Work on editing portion of release 3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,10 +2,10 @@
 
 puts "Please Enter the Following"
 
-puts "Clients Name:"
+puts "Client Name:"
 name = gets.chomp
 
-puts "Clients Age:"
+puts "Client Age:"
 age = gets.chomp.to_i
 
 puts "Number of children:"
@@ -32,23 +32,37 @@ customer_information = {
 }
 
 #print hash back to screen
+puts "Here is the completed profile."
 puts customer_information
 
 #last chance to edit data
 puts "Is there anything you would like to edit? Enter 'done' to complete"
 user_response = gets.chomp.downcase
 
-if user_response == 
+edited_response = " "
 
+
+if user_response == "y"
+	puts "Which category would you like to change?"
+	edited_response == gets.chomp.downcase
 else user_response == "done"
 
 end
 
-if edit_response == "client name"
+ #if user elects to edit, assign new value to symbol
+if edited_response == "client name"
+	puts "what would you like to change it to?"
+	edit = gets.chomp.downcase
 	customer_information = {
-		:name => edit_response
+		:name => edit
 	}
-else edit_response == "done"
+elsif edited_response == "client age"
+	puts "what would you like to change it to?"
+	edit = gets.chomp.downcase
+	customer_information = {
+		:age => edit
+	}
+else 
 end
 
 puts customer_information
