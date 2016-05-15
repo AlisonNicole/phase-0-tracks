419b305825ce4b11194355cb7abafafc8232b2ce
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-14 22:52:31 -0700

    Complete release 0

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,52 @@
 #ask user for real name (string)
 #create a fake name (string)
 	#swap first and last name
-	#change all the vowels to the next voewl in 'aeiou'
+	#change all the vowels to the next vowel in 'aeiou'
 	#change all consonants to the next consonant in the alphabet	
 
-puts "Please enter your real name."
-real_name = gets.chomp.downcase
+puts "Please enter your first name."
+first_name = gets.chomp.downcase
 
-#swap first and last name
+puts "Please enter your last name."
+last_name = gets.chomp.downcase
 
-#create the array
-#split_name << real_name
-split_name = real_name.split
-#reverse the order of the array
-split_name.reverse
+def encrypt(name)
+	vowel = "aeiou"
+	consonant = "bcdfghjklmnpqrstvwxyz"
+	index = 0
+	
+	while index < name.length
+    	letter = name[index]
+    	position = vowel.index(letter)
+    	
+ #this is checking if the letter is contained within vowel.   	
+    	if position != nil
+    		if
+    			name[index] == "u"
+      			name[index] = "a"
+    		else
+      			name[index] = vowel[position+1]
+#this will run if the letter is a consonant  
+			end
+      	else 
+      	position = consonant.index(letter)
+      		if 
+      			name[index] == "z"
+      			name[index] = "b"
+      		else
+      			name[index] = consonant[position+1]
+      		end
+      		
+		end
+	index += 1
+    end
+    
+	return name.capitalize!
 
-#----------anything above this is working------------------------
+end
+
+puts "your new name is #{encrypt(last_name)} #{encrypt(first_name)}"
+
+
+
+
