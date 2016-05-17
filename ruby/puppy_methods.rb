ea9c725e875ed66259c1cdf00f1f36ba87492785
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-17 14:40:23 -0700

    Complete 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+ 	puts "Initializing new puppy instance..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -20,19 +24,50 @@ class Puppy
   def sit
   	puts "Puppy is sitting!"
   end
+
 end
 
 
 #--DRIVER CODE-----
 
-della = Puppy.new
+# della = Puppy.new
+# bailey = Puppy.new
+# della.fetch("ball")
+# della.speak(4)
+# della.roll_over
+# della.dog_years(3)
+# della.sit
 
-#della.fetch("ball")
+class Mustang
+	def initialize
+		puts "Initializing Mustang"
+	end
 
-#della.speak(4)
+	def roar_engine(n)
+		puts "Vroom " * n
+	end
 
-#della.roll_over
+	def flash_lights
+		puts "Flashing lights!"
+	end
+end
 
-#della.dog_years(3)
+car_arr = Array.new
 
-#della.sit
+(1..50).each { car_arr.push(Mustang.new)}
+
+car_arr.each do |car|
+	car.roar_engine(2)
+	car.flash_lights
+end
+
+#--DRIVER CODE-----
+#sexy_car = Mustang.new
+
+# sexy_car.roar_engine(3)
+# sexy_car.flash_lights
+
+
+
+
+
