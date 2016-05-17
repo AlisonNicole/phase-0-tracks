0a72fdc09aa800f194b6e5716c22b49c73f15e6c
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-16 21:32:59 -0700

    Complete 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------------------------
+# Caninus Youngus
+
+# CHARACTERISTICS ----------------
+# Name: varies
+# Age: varies
+# Friendly: yes
+# Color: varies
+
+
+# BEHAVIOR -----------------------
+# Hop
+# Snuggle
+# Bite
+# Bark
