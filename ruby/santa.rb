51d4b49702e5e75b51c0b1283ac20cfee1c0ba2c
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-19 15:57:29 -0700

    Complete release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -11,9 +15,42 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
+	
+	#setter methods
+	def celebrate_birthday
+		@age += 1 
+	end 
 
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking << @reindeer_ranking.delete(reindeer_name)
+	end 
+	
+	#getter methods
+	def age
+		@age
+	end 
+	
+	def ethnicity
+		@ethnicity
+	end 
 end
 
-santa_a = Santa.new
 
-santa_a.eat_milk_and_cookies("snickerdoodle")
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# driver code with diverse initializations
+# santas << Santa.new("female", "aethnicity")
+santa = Santa.new("male", "Chinese")
+#p santa
+santa.celebrate_birthday
+puts "Santa is now #{santa.age}"
+#p santa
+santa.get_mad_at("Vixen")
+p santa
