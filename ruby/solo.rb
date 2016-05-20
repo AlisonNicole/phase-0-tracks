386f75f1cea8903927806fa75caf774c0aa13b50
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-19 20:02:25 -0700

    Create user interface for Otter class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,9 +1,35 @@
-#Ipod class
+#Otter class
 #   Attributes
 #	   -color
 #      -size
-#      -year
+#      -age
 #  Methods
-#      - connect headphones
-#      - download song- takes an argument
-# 	   - play song will take an argument
+#      - squeak x number of times where x is an argument
+#      - dive
+# 	   - eat fish
+
+class Otter
+	attr_reader :legs
+	attr_accessor :color, :size, :age
+
+	def initialize
+		@color
+		@size
+		@age
+		@legs = 4
+	end 
+end
+
+
+
+#------------USER INTERFACE--------------------
+#Ask user how many otters they would like to make? 
+puts "How many otters should we make today?"
+number_otters = gets.chomp
+
+#create as many instances of otter as the user input
+otters_created = 0 
+
+until otters_created == number_otters
+	
+end
