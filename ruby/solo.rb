db16449f8ec97bade0b418a5b29ea600261ff9bf
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-19 19:49:44 -0700

    Initial Commit

---
+++ ruby/solo.rb
@@ -0,0 +1,9 @@
+#Ipod class
+#   Attributes
+#	   -color
+#      -size
+#      -year
+#  Methods
+#      - connect headphones
+#      - download song- takes an argument
+# 	   - play song will take an argument
