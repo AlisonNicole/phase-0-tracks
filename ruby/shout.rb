ac9fe4ee1e2d2116bbcc53579f2d489405ab260e
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-21 15:49:08 -0700

    Complete 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,42 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+#---------Old Shout Module---------------------
 
-  def self.yelling_happily(words)
-  	puts "This is a happy #{words}!" 
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	puts "This is a happy #{words}!" 
+#   end
+# end
+
+# p Shout.yell_angrily("No")
+# p Shout.yelling_happily("Yay")
+
+#---------New Shout Module---------------------
+module Shout
+	def yell_angrily(words)
+		words + "!!!"
+	end
+
+	def yell_happily(words)
+		puts "This is a happy #{words}!"
+	end
 end
 
-p Shout.yell_angrily("No")
-p Shout.yelling_happily("Yay")
+class Mom
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+mom = Mom.new
+p mom.yell_angrily("Do your homework")
+p mom.yell_happily("day")
+
+
+teacher = Teacher.new
+p teacher.yell_angrily("Your time is up")
+p teacher.yell_happily("test")
