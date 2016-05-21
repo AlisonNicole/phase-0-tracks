12327f9a6e2a4f50d2b869e992302459e65359a5
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-20 21:18:52 -0700

    Complete 6.4

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,35 +1,63 @@
 #Otter class
 #   Attributes
-#	   -color
+#	   -name
 #      -size
 #      -age
 #  Methods
 #      - squeak x number of times where x is an argument
 #      - dive
-# 	   - eat fish
+# 	   - eat fish-take type of fish as argument
 
 class Otter
-	attr_reader :legs
-	attr_accessor :color, :size, :age
+	attr_reader
+	attr_accessor :name, :color, :age
 
-	def initialize
-		@color
-		@size
-		@age
-		@legs = 4
-	end 
+	def initialize(name)
+		@name = name
+		puts "#{@name} is ready to go!"
+	end
+
+	def dive
+		puts "Dive, Dive, Dive!"
+	end
+
+	def eat_fish(type)
+		puts "Your otter just ate a #{type}"
+	end
+
 end
-
 
 
 #------------USER INTERFACE--------------------
-#Ask user how many otters they would like to make? 
-puts "How many otters should we make today?"
-number_otters = gets.chomp
+#to create an instance 
+# otter = Otter.new
+# p otter
 
-#create as many instances of otter as the user input
-otters_created = 0 
 
-until otters_created == number_otters
-	
+# 10.times do
+#   santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+#   santa.age = rand(0..140)
+#   santas << santa
+# end
+#----------create as many instances of otter as the user input
+
+list_of_otters= []
+
+puts "How many otters would you like to create?"
+number_of_otters = gets.chomp.to_i 
+puts "What name is this group of otters?"
+input_name = gets.chomp
+puts "How old are your otters?"
+age = gets.chomp
+puts "What color are your otters?"
+color = gets.chomp
+
+number_of_otters.times do 
+	otter= Otter.new(input_name)
+	otter.age= age
+	otter.color= color
+	list_of_otters << otter
 end
+
+p list_of_otters
+
