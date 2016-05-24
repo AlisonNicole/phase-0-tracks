2c4b0924ec7db9d8e8ebd69eb0dc5e80c345aecf
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-23 21:44:47 -0700

    Create reverse function

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+//pseudocode a function that takes a string as a parameter and reverses the string.
+
+
+//find the length of the string
+// loop through the string
+//return each letter in the string starting from the end and working towards the front
+
+
+var str = "hello"
+
+for (var i = str.length; i > -1; i--) {
+	console.log(str[i]);
+}
+
+/// -------------DRIVER CODE ----------------------
+
