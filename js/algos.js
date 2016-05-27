8e8d5163ca41a0e3295682c0a2a6b330fe7d3f1a
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-27 00:15:15 -0700

    Complete logic for release 0

--- js/algos.js
+++ js/algos.js
@@ -4,3 +4,14 @@
 //sort the array based off of longest to shortest lengths
 //return the item at index 0, which should be the longest string
 
+var words = ["long phrase","longest phrase","longer phrase"]
+
+	if (words[0].length > words[1].length && words[0].length > words[2].length) {
+		console.log(words[0]);
+	}
+	else if (words[1].length > words[0].length && words[1].length > words[2].length) {
+		console.log(words[1]);
+	}
+	else {
+		console.log(words[2]);
+	}	
