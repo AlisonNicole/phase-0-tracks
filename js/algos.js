4f59930cdeb30d184b0ee7fbac7f43cba7c95bd7
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-26 21:33:52 -0700

    Initial commit for 7.3

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+
+//Release 0 
+//iterate through the array finding the length of each item 
+//sort the array based off of longest to shortest lengths
+//return the item at index 0, which should be the longest string
+
