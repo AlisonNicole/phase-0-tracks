ae92887d4d904d20bbcf7e2532536c81a88cc4a8
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-30 12:23:35 -0700

    Complete logic for release 1

--- js/algos.js
+++ js/algos.js
@@ -6,6 +6,8 @@
 
 var words = ["long phrase","longest phrase","longer phrase"]
 
+function longest_word(){
+
 	if (words[0].length > words[1].length && words[0].length > words[2].length) {
 		console.log(words[0]);
 	}
@@ -15,3 +17,26 @@ var words = ["long phrase","longest phrase","longer phrase"]
 	else {
 		console.log(words[2]);
 	}	
+}
+
+longest_word(words);
+
+//Release 1
+//Separate objects at the top
+//compare the values for each object for equality
+//	- use key-values to access each value for comparison
+object1 = {name: "Steven", age: 58};
+object2 = {name: "Tamir", age: 54};
+
+function comparison(object1, object2) {
+	if (object1['name'] == object2['name']) {
+		return true;
+}	else if (object1['age'] == object2['age']) {
+		return true;
+}	else {
+		return false;
+}
+}
+
+console.log(comparison(object1, object2));
+
