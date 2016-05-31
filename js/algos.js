7adc32144fba92372a35f03a55660cfd01b5c676
Alison Mueller <Alisonnmueller@gmail.com>
2016-05-30 20:18:24 -0700

    Complete 7.3

--- js/algos.js
+++ js/algos.js
@@ -40,3 +40,26 @@ function comparison(object1, object2) {
 
 console.log(comparison(object1, object2));
 
+//Release 2
+//function with integer as argument- for example, 3
+//build a single random word, taking letters from the alphabet at random
+//build 3 strings
+//push them into an array
+//return array of strings
+
+//I was not able to complete this release. But my goal was to pull items from the alphabet array perhaps by indexing from the random number that was generated.
+
+
+// var alphabet = ["a","b", "c", "d", "e", "f", "g", "h", "i", 'j', "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// function random_strings(x) {
+// 	for (x) {
+
+// 	}
+// }
+
+// random_strings(3)
+
+// for (var i = 0; i < str.length; i++) {
+// 	console.log(str[i]);
+// }
