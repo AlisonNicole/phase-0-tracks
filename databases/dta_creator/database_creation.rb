5822bbe7a63e7f93ac192b51e36d3078fb392913
Alison Mueller <Alisonnmueller@gmail.com>
2016-06-04 19:44:04 -0700

    Create database creation file

---
+++ databases/dta_creator/database_creation.rb
@@ -0,0 +1,24 @@
+#A way to change the piece of data included in a paragraph with the correct dog, colors, markings, and groups. 
+
+#This will set up the database and any necessary tables
+
+require 'sqlite3'
+#This is where i will have to require_relative the other file i make
+
+
+# create SQLite3 database
+db = SQLite3::Database.new("dogbreeds.db")
+
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs(
+    id INTEGER PRIMARY KEY,
+    breedname VARCHAR(255),
+    colors INT,
+    markings INT,
+    group_id INT,
+    FOREIGN KEY (group_id) REFERENCES groups(id)
+  )
+SQL
+
+db.execute(create_table_cmd)
