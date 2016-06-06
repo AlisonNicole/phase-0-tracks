7b93284c4b6c2dda7bdb2435399bfc5c661ed5de
Alison Mueller <Alisonnmueller@gmail.com>
2016-06-05 20:04:16 -0700

    Added UI to take breed and return sentence with specific traits

--- databases/dta_creator/database_creation.rb
+++ databases/dta_creator/database_creation.rb
@@ -1,10 +1,12 @@
-#A way to change the piece of data included in a paragraph with the correct dog, colors, markings, and groups. 
+#This is a program designed to autofill a Detailed Trait Analysis(DTA) that is used in my current job. It is a prewritten template that is filled in depending on the breed of dog we are looking at. It is a very time consuming process that could be simplied if we work from a database of pieces that we can pull from.
+
+# I couldn't add all 250 dog breeds along with their markings, but added 5 test dogs to make sure it was working. If it were to be more in depth, I would like to add a table that lists each of the colors and markings carried by each breed as well as additional history about the breed that can then be referred to as well. 
+
+
 
 #This will set up the database and any necessary tables
 
 require 'sqlite3'
-#This is where i will have to require_relative the other file i make
-
 
 # create SQLite3 database
 db = SQLite3::Database.new("dogbreeds.db")
@@ -56,20 +58,36 @@ end
 # add_dog(db, "Airedale Terrier", 2, 0, 5)
 # add_dog(db, "Akita", 9, 5, 7)
 # add_dog(db, "Alaskan Malamute", 9, 0, 7)
+
+def get_info (db, breed)
+	dog_info = db.execute("SELECT * FROM dogs WHERE 	breedname='" + breed + "'")
+
+	dog_info.each do |dog|
+	puts "The #{dog['breedname']} has #{dog['colors']} colors and #{dog['markings']} markings."
+	end
+
+end
 #---------USER INTERFACE------------
+
 #print a list of breeds available to choose from
-
 list_dogs = db.execute("SELECT dogs.breedname, groups.name FROM dogs JOIN groups ON dogs.group_id = groups.id")
-puts list_dogs.class
-p list_dogs
-
 
 puts "Here is a list of breeds to choose from:"
 list_dogs.each do |dog|
 puts "#{dog['breedname']} is in the #{dog['name']} group."
 end 
 
-# puts "What breed would you like general information on?"
-# response = gets.chomp
+puts "What breed would you like more information on?"
+response = gets.chomp.split.each{|i| i.capitalize!}.join(' ')
+p response
+
+get_info(db, response)
+
+#response will provide a breed name
+#use that name to collect the name, colors, and markings of that breed
+
+#insert the info from the table into the paragraph about the breed.
+
+
 
 
