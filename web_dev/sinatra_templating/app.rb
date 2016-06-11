2cf975cce4a1a8a1666bd5fbd93cef991cbe9419
Alison Mueller <Alisonnmueller@gmail.com>
2016-06-10 19:26:20 -0700

    Complete 9.5 mandatory pairing

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -10,11 +10,23 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @questions = db.execute("SELECT * FROM questions")
   erb :home
 end
 
 get '/students/new' do
   erb :new_student
+end
+
+#this is not working quite yet
+get '/questionnaire' do 
+	erb :questionnaire
+end
+
+get '/locations' do 
+	@students = db.execute("SELECT * FROM students WHERE campus= 'CHI'")
+	erb :locations
+
 end
 
 # create new students via
@@ -23,5 +35,12 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/results' do
+	db.execute("INSERT INTO questions (hoursofsleep, actualhours VALUES (?,?)", [params['hoursofsleep'].to_i, params['actualhours'].to_i])
+	redirect "/"
+end 
+
+ 
 
 # add static resources
